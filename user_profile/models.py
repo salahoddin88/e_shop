1eae385cdb3ceb17a27fa6c8e5b8021590e53e44
Salahuddin Shaikh <50991921+salahuddinshaikh@users.noreply.github.com>
2022-01-14 21:05:47 +0530

    Signals

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,3 +1,4 @@
+from django.dispatch import receiver
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -12,4 +13,19 @@ class UserProfile(models.Model):
         """ String Representation of object"""
         return str(self.user)
 
-    
+
+# Signals 
+""" When User model is created or save create_profile, save_profile will receive a signal  """
+from django.db.models.signals import post_save
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        print('create_profile signal ')
+        UserProfile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    print('updated...', instance)
